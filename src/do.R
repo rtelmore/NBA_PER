3b96f1cef6023ac66898e56ba70aa4ca913597d2
Ryan Elmore <rtelmore@gmail.com>
2011-03-20 23:07:19 -0600

    changed the scraper

--- src/do.R
+++ src/do.R
@@ -13,55 +13,58 @@ wd.path <- "~/Side_Projects/Sports/NBA/NBA_PER/"
 source(paste(wd.path, "src/load.R", sep = ""))
 
   
-r.scrape <- system.time(source(paste(wd.path, "src/scrape.R", sep = "")))
 
 out.string <- paste(Sys.time(), " -- Starting", sep = "")
 print(out.string)
 
 
-wd.path <- "~/Side_Projects/Sports/Scraping/"
+wd.path <- "~/Side_Projects/Sports/NBA/NBA_PER/"
 
-years <- seq(2005, 2009, by = 1)
+# teams <- c("hawks", "celtics")
+teams <- c("hawks", "celtics", "bobcats", "bulls", "cavaliers", "mavericks",
+          "nuggets", "pistons", "warriors", "rockets", "pacers", "clippers",
+          "lakers", "grizzlies", "heat", "bucks", "timberwolves", "nets", 
+          "hornets", "knicks", "thunder", "magic", "sixers", "suns", "blazers",
+          "kings", "spurs", "raptors", "jazz", "wizards")
 
-df.final <- data.frame()
-for (i in 1:5){
-  url.tmp <- paste("http://www.majorleaguesoccer.com/stats/", years[i], 
-    sep = "")
-  mls.url <- paste(url.tmp, "/reg", sep = "")
-  doc <- htmlTreeParse(mls.url, useInternalNodes=T)
+teams <- c("bulls")
+df.statistics <- data.frame()
+
+for (team in teams){
+  url.tmp <- paste("http://www.nba.com/", team, sep = "")
+  nba.url <- paste(url.tmp, "/stats/", sep = "")
+  # celts <- "~/Side_Projects/Sports/NBA/NBA_PER/data/celts.html"
+  doc <- htmlTreeParse(nba.url, useInternalNodes=T)
+  # doc <- htmlTreeParse(celts, useInternalNodes=T)
   nset.stats <- getNodeSet(doc, 
-    "//div/table[@class='stats sortable-first team-totals']")
-  table.stats <- lapply(nset.stats, readHTMLTable, header = FALSE)
-  nset.attendance <- getNodeSet(doc, 
-    "//div/table[@class='sortable-first stats attendance']")
-  table.attendance <- lapply(nset.attendance, readHTMLTable, header = FALSE)
-  df.stats <- table.stats[[1]]
-  df.attendance <- table.attendance[[1]]
-  df.stats[order(df.stats$V1), ]
-  df.attendance[order(df.attendance$V1), ]
-  df.total <- merge(df.stats, df.attendance, by = "V1", all = TRUE)
-  df.total$year <- rep(years[i], length(df.total$V1))  
-  df.final <- rbind(df.final, df.total)
-  out.string.2 <- paste(Sys.time(), paste(" -- Year: ", years[i], sep = ""), 
+    "//table[@class=' gSGTable']")
+  table.stats <- lapply(nset.stats, readHTMLTable, header = F)
+  stats.one <- table.stats[[1]][-(1:3), ]
+  names(stats.one) <- table.stats[[1]][3, ]
+  nRecords <- dim(stats.one)[1]
+  stats.one$team <- team
+  stats.two <- table.stats[[2]][-(1:3), -(1:2)]
+  names(stats.two) <- table.stats[[2]][3, -(1:2)]
+  df.team <- cbind(stats.one[-nRecords, ], stats.two[-nRecords, -1])
+  df.statistics <- rbind(df.statistics, df.team)
+  out.string.2 <- paste(Sys.time(), paste(" -- Team: ", team, sep = ""), 
                       sep = "")
   print(out.string.2)  
 }
 
-# source(paste(wd.path, "load.R", sep=""))
-
-out.file <- paste(wd.path, "data/R_", 
+out.file <- paste(wd.path, "data/nba_", 
               paste(format(Sys.Date(), "%Y%m%d"), ".csv", sep = ""), sep ="")
-write.table(df.final, file = out.file, sep = ";", col.names = FALSE)
+write.table(df.statistics, file = out.file, sep = ";", col.names = FALSE)
 
 final.string <- paste(Sys.time(), " -- Finished :)", sep = "")
 print(final.string)
 
 
-p + geom_line(lty = 2) + 
-  geom_point(colour="grey60", size = 4) +
-  geom_point(aes(colour = type)) +
-  scale_x_discrete("timed category") +
-  scale_y_continuous("time (in sec)", limits = c(0, 3)) +
-  scale_colour_manual("language", values = c("forestgreen", "darkred"))
-
-ggsave(file = paste(wd.path, "fig/time.png", sep = ""), hei = 7, wid = 7)
+# p + geom_line(lty = 2) + 
+#   geom_point(colour="grey60", size = 4) +
+#   geom_point(aes(colour = type)) +
+#   scale_x_discrete("timed category") +
+#   scale_y_continuous("time (in sec)", limits = c(0, 3)) +
+#   scale_colour_manual("language", values = c("forestgreen", "darkred"))
+# 
+# ggsave(file = paste(wd.path, "fig/time.png", sep = ""), hei = 7, wid = 7)
