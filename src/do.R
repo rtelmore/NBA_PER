62ea1a530f285a0cab41c79adb38908984b625bc
Ryan Elmore <rtelmore@gmail.com>
2011-03-21 23:13:09 -0600

    modified scraper; still need to reconcile players

--- src/do.R
+++ src/do.R
@@ -10,61 +10,58 @@
 wd.path <- "~/Side_Projects/Sports/NBA/NBA_PER/"
 
 ## Dependencies:
-source(paste(wd.path, "src/load.R", sep = ""))
+source(paste(wd.path, "src/load.R", sep=""))
 
   
 
-out.string <- paste(Sys.time(), " -- Starting", sep = "")
+out.string <- paste(Sys.time(), " -- Starting", sep="")
 print(out.string)
 
 
-wd.path <- "~/Side_Projects/Sports/NBA/NBA_PER/"
-
-# teams <- c("hawks", "celtics")
+# teams <- c("hawks", "celtics", "bulls")
 teams <- c("hawks", "celtics", "bobcats", "bulls", "cavaliers", "mavericks",
           "nuggets", "pistons", "warriors", "rockets", "pacers", "clippers",
           "lakers", "grizzlies", "heat", "bucks", "timberwolves", "nets", 
           "hornets", "knicks", "thunder", "magic", "sixers", "suns", "blazers",
           "kings", "spurs", "raptors", "jazz", "wizards")
 
-teams <- c("bulls")
 df.statistics <- data.frame()
 
 for (team in teams){
-  url.tmp <- paste("http://www.nba.com/", team, sep = "")
-  nba.url <- paste(url.tmp, "/stats/", sep = "")
-  # celts <- "~/Side_Projects/Sports/NBA/NBA_PER/data/celts.html"
-  doc <- htmlTreeParse(nba.url, useInternalNodes=T)
-  # doc <- htmlTreeParse(celts, useInternalNodes=T)
-  nset.stats <- getNodeSet(doc, 
-    "//table[@class=' gSGTable']")
-  table.stats <- lapply(nset.stats, readHTMLTable, header = F)
-  stats.one <- table.stats[[1]][-(1:3), ]
-  names(stats.one) <- table.stats[[1]][3, ]
-  nRecords <- dim(stats.one)[1]
-  stats.one$team <- team
-  stats.two <- table.stats[[2]][-(1:3), -(1:2)]
-  names(stats.two) <- table.stats[[2]][3, -(1:2)]
-  df.team <- cbind(stats.one[-nRecords, ], stats.two[-nRecords, -1])
+  url.tmp <- paste("http://www.nba.com/", team, sep="")
+  nba.url <- paste(url.tmp, "/stats/", sep="")
+  if(team %in% c("bulls", "raptors")) {
+    table.stats <- readHTMLTable(nba.url)
+    stats.one <- table.stats[1]$`NULL`[-1, ]
+    names(stats.one) <- table.stats[1]$`NULL`[1, ]
+    stats.two <- table.stats[2]$`NULL`[-1, ]
+    names(stats.two) <- table.stats[2]$`NULL`[1, ]
+    df.team <- cbind(stats.one, stats.two[, -(2:3)])
+    df.team$team <- team
+  }
+  else {
+    doc <- htmlTreeParse(nba.url, useInternalNodes=T)
+    nset.stats <- getNodeSet(doc, "//table[@class=' gSGTable']")    
+    table.stats <- lapply(nset.stats, readHTMLTable, header=F)
+    stats.one <- table.stats[[1]][-(1:3), ]
+    names(stats.one) <- table.stats[[1]][3, ]
+    nRecords <- dim(stats.one)[1]
+    stats.two <- table.stats[[2]][-(1:3), -(2:3)]
+    names(stats.two) <- table.stats[[2]][3, -(2:3)]
+    df.team <- cbind(stats.one[-nRecords, ], stats.two[-nRecords, ])
+    df.team$team <- team
+  }
   df.statistics <- rbind(df.statistics, df.team)
-  out.string.2 <- paste(Sys.time(), paste(" -- Team: ", team, sep = ""), 
-                      sep = "")
+  out.string.2 <- paste(Sys.time(), paste(" -- Team: ", team, sep=""), 
+                      sep="")
   print(out.string.2)  
 }
 
 out.file <- paste(wd.path, "data/nba_", 
-              paste(format(Sys.Date(), "%Y%m%d"), ".csv", sep = ""), sep ="")
-write.table(df.statistics, file = out.file, sep = ";", col.names = FALSE)
+                  paste(format(Sys.Date(), "%Y%m%d"), ".csv", sep=""), 
+                  sep ="")
+write.table(df.statistics, file=out.file, sep=";", col.names=FALSE)
 
-final.string <- paste(Sys.time(), " -- Finished :)", sep = "")
+final.string <- paste(Sys.time(), " -- Finished :)", sep="")
 print(final.string)
 
-
-# p + geom_line(lty = 2) + 
-#   geom_point(colour="grey60", size = 4) +
-#   geom_point(aes(colour = type)) +
-#   scale_x_discrete("timed category") +
-#   scale_y_continuous("time (in sec)", limits = c(0, 3)) +
-#   scale_colour_manual("language", values = c("forestgreen", "darkred"))
-# 
-# ggsave(file = paste(wd.path, "fig/time.png", sep = ""), hei = 7, wid = 7)
